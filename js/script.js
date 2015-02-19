a8df755ee9b6dfcbc9f6825fc3bcec7dc13a48f7
Keddon Campbell <mrkfed101@gmail.com>
2015-02-19 17:55:01 -0500

    Problem 2
    using if statement to replicate a login process

--- js/script.js
+++ js/script.js
@@ -17,3 +17,28 @@ if (units == " degrees Celsius"){ //determines what calculation to use
     results = (degrees - 32)/1.8; //calculates fahrenheit to celsius
 }else results = (degrees * 1.8) + 32; //calculates celsius to fahrenheit
 console.log("The temperature is "+ results +" "+ units);
+
+/* Problem 2
+ Make sure the user has the correct username and password.
+  If the username doesn’t match then a specific message for that should be printed to the console.
+  If the password doesn’t match a message should appear for that, etc. Only one error message should appear.
+*/
+
+var userName = "Username";
+var userPass = "Password";
+
+var nameCheck = prompt("Please enter user name");
+var passCheck = prompt("please enter password");
+
+
+
+if ((userName === nameCheck) && (userPass === passCheck)) {
+    console.log("Welcome "+userName+ ", How are you doing today");
+}else if ((userName != nameCheck)) {
+    console.log("User not found. Please Try Again.");
+}else if ((userName === nameCheck) && (userPass != passCheck)){
+    console.log("Password does not match our records, please try again.");
+}else {
+    console.log("Error 404, You broke something.")
+}
+
