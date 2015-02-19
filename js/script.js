2ca04e1e2a5a3e2b2e93ffc9a7a9b5a9fb7170a2
Keddon Campbell <mrkfed101@gmail.com>
2015-02-19 18:14:04 -0500

    Problem 3
    Creating an array to store the data of all four tire separately in one
    object.

--- js/script.js
+++ js/script.js
@@ -24,21 +24,37 @@ console.log("The temperature is "+ results +" "+ units);
   If the password doesn’t match a message should appear for that, etc. Only one error message should appear.
 */
 
-var userName = "Username";
-var userPass = "Password";
-
-var nameCheck = prompt("Please enter user name");
+var userName = "Username"; //stored user information
+var userPass = "Password"; // stored user data
+var nameCheck = prompt("Please enter user name"); //
 var passCheck = prompt("please enter password");
 
-
-
-if ((userName === nameCheck) && (userPass === passCheck)) {
+if ((userName === nameCheck) && (userPass === passCheck)) {  //if both are true then the user is logged in
     console.log("Welcome "+userName+ ", How are you doing today");
-}else if ((userName != nameCheck)) {
+}else if ((userName != nameCheck)) {                         //checks for name issues if login failed
     console.log("User not found. Please Try Again.");
-}else if ((userName === nameCheck) && (userPass != passCheck)){
+}else if ((userName === nameCheck) && (userPass != passCheck)){    //checks for password issue if login failed
     console.log("Password does not match our records, please try again.");
-}else {
+}else {                                                       //an automatic debugger if something else happens.
     console.log("Error 404, You broke something.")
 }
 
+/* Problem 3
+ To meet to maintenance standards a car’s front two tires should have the
+ same pressure and the back two tires should have the same pressure.
+ But the front tires and the rear tires can have different pressure than each other,
+  so it is not necessary for all four tires’ pressure to be the same. Create a single conditional
+  that would determine if the tires of a given car are up to spec.
+ */
+
+var tire = [0,0,0,0];
+var frontLeft = (tire[0] = prompt("what is the pressure of the front left tire?"));
+var frontRight = (tire[1] = prompt("what is the pressure of the front right tire?"));
+var rearLeft = (tire[2] = prompt("what is the pressure of the rear left tire?"));
+var rearRight = (tire[3] = prompt("what is the pressure of the rear right tire?"));
+
+if ((frontLeft === frontRight) && (rearLeft === rearRight)){
+    console.log("Looks like your tires are good here!");
+}else{
+    console.log("Get your tires checked out right away!");
+}
